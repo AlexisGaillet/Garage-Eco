52775519231033d99c3b9f48997fcc6b5cf4e207
AlexisGaillet <alexisgaillet36@gmail.com>
2022-11-26 13:18:24 +0100

    fix update

--- views/register.php
+++ views/register.php
@@ -1,25 +1,26 @@
 <main class="mainRegisterLogin flex-column-center">
     <h2 class="pageTitle">Inscription</h2>
-    <form method="post" class="registerLoginForm flex-column" novalidate>
+    <form method="post" class="registerLoginForm flex-column">
         <ul>
-            <li><input type="text" placeholder="Prénom" value="<?=$firstname ?? ''?>" name="firstname" class="flex-row" required></li>
+            <li><input type="text" placeholder="Prénom" value="<?=$firstname ?? ''?>" name="firstname" class="flex-row" required><span class="textRed font-size-1-6rem">*</span></li>
             <p class="errorText"><?=$error['firstname'] ?? ''?></p>
 
-            <li><input type="text" placeholder="Nom" value="<?=$lastname ?? ''?>" name="lastname" class="flex-row" required></li>
+            <li><input type="text" placeholder="Nom" value="<?=$lastname ?? ''?>" name="lastname" class="flex-row" required><span class="textRed font-size-1-6rem">*</span></li>
             <p class="errorText"><?=$error['lastname'] ?? ''?></p>
 
-            <li><input type="email" placeholder="Email" value="<?=$mail ?? ''?>" name="mail" class="flex-row" required></li>
+            <li><input type="email" placeholder="Email" value="<?=$mail ?? ''?>" name="mail" class="flex-row" required><span class="textRed font-size-1-6rem">*</span></li>
             <p class="errorText"><?=$error['mail'] ?? ''?></p>
 
-            <li><input type="password" placeholder="Mot de passe" name="password" class="flex-row" required></li>
-            <li><input type="password" placeholder="Confirmation du mot de passe" name="confirmPassword" class="flex-row" required></li>
+            <li><input type="password" placeholder="Mot de passe" name="password" class="flex-row" required><span class="textRed font-size-1-6rem">*</span></li>
+            <li><input type="password" placeholder="Confirmation du mot de passe" name="confirmPassword" class="flex-row" required><span class="textRed font-size-1-6rem">*</span></li>
             <p class="errorText"><?=$error['password'] ?? ''?></p>
         </ul>
         <div class="acceptConditionsContainer flex-row">
             <input type="checkbox" name="acceptConditions" id="acceptConditions" <?= (isset($acceptConditions) && $acceptConditions == true) ? 'checked' : ''; ?> required>
-            <label for="acceptConditions">J'ai lu et j'accepte <a href="XXXXXXXXXXXXX">les conditions</a></label>
+            <label for="acceptConditions">J'ai lu et j'accepte <a href="XXXXXXXXXXXXX">les conditions</a></label><span class="textRed font-size-1-6rem">*</span>
         </div>
         <p class="errorText"><?=$error['acceptConditions'] ?? ''?></p>
+        <p class="font-size-0-9rem"><span class="textRed">*</span>(Champs obligatoire)</p>
         <input type="submit" value="Inscription" class="registerLoginButton">
         <div class="linkUnderButtonContainer">
             <p>Vous avez déjà un compte ?</p>
